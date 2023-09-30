b3899b2061b967cebff23dec00f74f3c00c7a0c5
dorikan <ci06122004@gmail.com>
2023-09-30 19:18:38 +0300

    added svg sace. vlad must make some changes

--- evacuate_us/services/tmp.py
+++ evacuate_us/services/tmp.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+from matplotlib import image
 
 def image_to_array(path):
     open_cv_image = cv2.imread(path)
@@ -9,7 +9,13 @@ def image_to_array(path):
 
     for i in range(shape[0]):
         for j in range(shape[1]):
-            if sum(open_cv_image[i][j]) <= 70:
+            tmp = open_cv_image[i][j]
+            if tmp[0] < 95 and tmp[1] < 20 and tmp[2] < 95:
                 arr[i][j] = 1
+                open_cv_image[i][j] = [0., 0., 0.]
+            else:
+                open_cv_image[i][j] = [255., 255., 255.]
+
+    image.imsave("test.svg", format="svg", arr=open_cv_image)
 
     return arr.tolist()
